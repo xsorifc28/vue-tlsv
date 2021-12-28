c52e5532a669956b43e159c5755b54dca367b5e9
Samed Ozdemir <samed@xsor.io>
2021-12-28 12:57:32 -0500

    feat: Remove polyfills, gzip served files

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const compression = require('compression');
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(compression());
 app.use(express.static('dist'));
 
 app.listen(port, () => console.log(`App is listening on port ${port}.`));
